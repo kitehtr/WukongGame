32c7686e691e49e4eeadba8ae734e458cc051c3c
kitehtr <keithtran0004@gmail.com>
2025-10-13 16:19:19 -0700

    removal of debug logs

--- Public/Enemy/AI/BTT_FindRandomLocation.cpp
+++ Public/Enemy/AI/BTT_FindRandomLocation.cpp
@@ -17,26 +17,16 @@ EBTNodeResult::Type UBTT_FindRandomLocation::ExecuteTask(UBehaviorTreeComponent&
 		if (APawn* const Enemy = Cont->GetPawn())
 		{
 			const FVector Origin = Enemy->GetActorLocation();
-			UE_LOG(LogTemp, Warning, TEXT("Origin: %s"), *Origin.ToString());
 
 			if (auto* const NavSystem = UNavigationSystemV1::GetCurrent(GetWorld()))
 			{
 				FNavLocation Loc;
 				if (NavSystem->GetRandomPointInNavigableRadius(Origin, SearchRadius, Loc))
 				{
-					UE_LOG(LogTemp, Warning, TEXT("Found valid Nav point: %s"), *Loc.Location.ToString());
 					OwnerComp.GetBlackboardComponent()->SetValueAsVector(GetSelectedBlackboardKey(), Loc.Location);
 					FinishLatentTask(OwnerComp, EBTNodeResult::Succeeded);
 					return EBTNodeResult::Succeeded;
 				}
-				else
-				{
-					UE_LOG(LogTemp, Warning, TEXT("No valid Nav point found within radius %.1f"), SearchRadius);
-				}
-			}
-			else
-			{
-				UE_LOG(LogTemp, Error, TEXT("NavSystem is NULL"));
 			}
 		}
 	}
